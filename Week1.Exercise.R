c5463b13d66f16b56ab8346d5ed1ec8cbfd67d24
ssun1116 <syk991116@korea.ac.kr>
2022-02-03 12:27:15 +0900

    update.0203

--- Week1.Exercise.R
+++ Week1.Exercise.R
@@ -16,3 +16,38 @@ colnames(cars)
 d = cars[,2]
 mean(d)
 which(cars$dist == 85)
+
+x <- 1:10
+y <- rnorm(10) # norrmal distribution.
+plot(x,y)
+plot(y)
+
+dat = femaleMiceWeights
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
+filename <- "femaleMiceWeights.csv" 
+download(url, destfile=filename)
+RNGkind("Mersenne-Twister", "Inversion", "Rejection")
+
+colnames(dat)
+dat[12,2]
+dat[11,]$Bodyweight
+length(dat$Bodyweight)
+mean(dat[13:24,]$Bodyweight)
+set.seed(1)
+dat[sample(13:24,1),]$Bodyweight
+
+library(dplyr)
+controls <- filter(dat, Diet == "chow")
+d = select(controls, Bodyweight)
+unlist(d)
+
+class(msleep)
+d = filter(msleep, order == "Primates")
+nrow(d)
+class(d)
+d = d %>% select(sleep_total)
+class(d)
+mean(d)
+mean(as.numeric(d))
+mean(unlist(d))
